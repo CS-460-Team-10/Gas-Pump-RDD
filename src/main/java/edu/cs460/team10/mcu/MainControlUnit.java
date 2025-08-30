9dd41915dfdf57323cf11804601fd436cfbf3d1c
Mkatbabader <Mkatbabader@gmail.com>
2025-08-30 00:49:35 -0600

    Update MainControlUnit.java

--- src/main/java/edu/cs460/team10/mcu/MainControlUnit.java
+++ src/main/java/edu/cs460/team10/mcu/MainControlUnit.java
@@ -2,15 +2,6 @@ package edu.cs460.team10.mcu;
 
 public class MainControlUnit {
     public static void main(String[] args) {
-        System.out.println("🚀 Main Control Unit started...");
-        System.out.println("Initializing components...");
-        
-        // Mock setup for this week
-        System.out.println("UI Socket: [pending]");
-        System.out.println("Payment API: [pending]");
-        System.out.println("Station API: [pending]");
-        System.out.println("Actuator + Sensors: [pending]");
 
-        System.out.println("✅ Setup complete.");
     }
 }
