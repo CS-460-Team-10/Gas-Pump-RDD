7bacc9177beb7fbdd8f7a1932f4e155b324f9b88
Alex Maynes <alex.maynes2001@gmail.com>
2025-09-09 09:41:39 -0600

    hub cleanup

--- src/hub.java
+++ src/hub.java
@@ -5,41 +5,60 @@ import java.util.Map;
 
 // Central location for all connections
 public class hub {
-    Map<Integer, ioPort> devices = new HashMap<>();
-    int numOfDevices = 4;
+    // Device IDs/mapping
+    private static final int SCREEN = 1;
+    private static final int FLOW_METER = 2;
+    //private static final int CARD_READER = 3; - Unused variable
+    private static final int HOSE = 4;
+    private final Map<Integer, ioPort> devices = new HashMap<>();
+    private final int deviceCount = 4;
 
+    // UI display markup codes
+    private static final String UI_WELCOME =
+        "t01/s1B/f1/c5/\"Welcome!\":t23/s3R/f1/c5/\"Use the card reader to begin your transaction.\":t45/s1B/f1/c5/\"*\"";
+
+    private static final String UI_SELECT_FUEL =
+        "t01/s2R/f1/c5/\"Payment approved. Select fuel type:\":t2/s3I/f1/c5/\"Unleaded\":"
+      + "t3/s3R/f1/c5/\"Confirm\":t4/s3I/f1/c5/\"Premium\":t6/s3I/f1/c5/\"Premium Plus\":t8/s3I/f1/c5/\"Gasoline\"";
+
+    private static final String UI_ATTACH_HOSE =
+        "t23/s2R/f1/c5/\"Attach the hose to your vehicle's \\n       gas tank to begin fueling.\":t45/s1B/f1/c5/\"*\"";
+
+    private static final String UI_FUELING =
+        "t23/s2R/f1/c5/\"Fueling in progress...\":t45/s1B/f1/c5/\"*\"";
+
+    private static final String UI_FINAL =
+        "t01/s2B/f1/c5/\"Transaction complete.\":t45/s3I/f1/c5/\"Thank you!\":t67/s1B/f1/c5/\"*\"";
+
+    // Flow meter commands
+    private static final String METER_ON = "FM1";
+    private static final String METER_OFF = "FM0";
+
+    // Hub client
     public hub() throws IOException, InterruptedException{
 
         // Connect to each device
-        for(int i = 1; i <= numOfDevices; i++){
+        for(int i = 1; i <= deviceCount; i++){
             ConnectToDevice(i);
         }
 
-        String displayScreen = "";
         String in;
-        int recipient;
-        String meter; // FM0 = Off; FM1 = On;
         boolean midDisco;
 
         // Cycle through screens on 5 second timer
         while (true) {
-            // Welcome Screen
-            displayScreen = "t01/s1B/f1/c5/\"Welcome!\":t23/s3R/f1/c5/\"Use the card reader to begin your transaction.\":t45/s1B/f1/c5/\"*\"";
-            recipient = 1;
-            send(displayScreen, recipient);
-            System.out.println("Hub Sending: \n" + displayScreen + "\n\n");
+            // *Welcome Screen*
+            send(UI_WELCOME, SCREEN);
+            System.out.println("Hub Sending: \n" + UI_WELCOME + "\n\n");
             in = get();
             while(in.charAt(5)!='3'){
                 in = get();
                 System.out.println("Hub Recieving: \n" + in);
             }
 
-            // Select Fuel Screen
-            displayScreen = "t01/s2R/f1/c5/\"Payment approved. Select fuel type:\":t2/s3I/f1/c5/\"Unleaded\":" +
-                    "t3/s3R/f1/c5/\"Confirm\":t4/s3I/f1/c5/\"Premium\":t6/s3I/f1/c5/\"Premium Plus\":t8/s3I/f1/c5/\"Gasoline\"";
-            recipient = 1;
-            send(displayScreen, recipient);
-            System.out.println("Hub Sending: \n" + displayScreen + "\n\n");
+            // *Select Fuel Screen*
+            send(UI_SELECT_FUEL, SCREEN);
+            System.out.println("Hub Sending: \n" + UI_SELECT_FUEL + "\n\n");
             midDisco = false;
             in = get();
             while(in.charAt(5)!='1' && (!in.contains("bps2/3") || !in.contains("bps4/3") || !in.contains("bps6/3") || !in.contains("bps8/3"))){ // Ensure button sequencing
@@ -47,60 +66,52 @@ public class hub {
                 System.out.println("Hub Recieving: \n" + in);
             }
 
-            // Attach Hose Screen
-            displayScreen = "t23/s2R/f1/c5/\"Attach the hose to your vehicle's \\n       gas tank to begin fueling.\":t45/s1B/f1/c5/\"*\"";
-            recipient = 1;
-            send(displayScreen, recipient);
-            System.out.println("Hub Sending: \n" + displayScreen + "\n\n");
+            // *Attach Hose Screen*
+            send(UI_ATTACH_HOSE, SCREEN);
+            System.out.println("Hub Sending: \n" + UI_ATTACH_HOSE + "\n\n");
             in = get();
             while(in.charAt(5)!='4'){
                 in = get();
                 System.out.println("Hub Recieving: \n" + in);
             }
-            recipient = 2;
-            meter = "FM1"; // Flow meter on
-            send(meter, recipient);
+            send(METER_ON, FLOW_METER);
 
-            // Fueling Screen
-            displayScreen = "t23/s2R/f1/c5/\"Fueling in progress...\":t45/s1B/f1/c5/\"*\"";
-            recipient = 1;
-            send(displayScreen, recipient);
-            System.out.println("Hub Sending: \n" + displayScreen + "\n\n");
+            // *Fueling Screen*
+            send(UI_FUELING, SCREEN);
+            System.out.println("Hub Sending: \n" + UI_FUELING + "\n\n");
             in = get();
             while(!in.equals("Port 4: Tank Full")){
-                if(in.equals("Port 4: Hose detached")) { midDisco = true; break; } // Mid fuel disconnect stop
-                if(in.contains("Gal Pumped:")) { 
-                    recipient = 4;
-                    send(in, recipient); 
+
+                if(in.equals("Port 4: Hose detached")) { 
+                    midDisco = true; break;  // Mid fuel disconnect stop - initiate emergency stop
                 }
+
+                if(in.contains("Gal Pumped:")) { 
+                    send(in, HOSE); 
+                }
+
                 in = get();
                 System.out.println("Hub Recieving: \n" + in);
             }
             // Clean fuel stop
             if(!midDisco){
-                recipient = 2;
-                meter = "FM0"; // Flow meter off
-                Thread.sleep(3000);
-                send(meter, recipient);
+                Thread.sleep(3000); // Wait so user can see fuel purchase amount
+                send(METER_OFF, FLOW_METER);
             } 
             // Mid fuel flow valve disconnect - emergency stop initiated
             else {
-                recipient = 2;
-                meter = "FM0"; // Flow meter off
-                send(meter, recipient);
+                send(METER_OFF, FLOW_METER);
                 Thread.sleep(3000);
             }
 
-            // Final Screen
-            displayScreen = "t01/s2B/f1/c5/\"Transaction complete.\":t45/s3I/f1/c5/\"Thank you!\":t67/s1B/f1/c5/\"*\"";
-            recipient = 1;
-            send(displayScreen, recipient);
-            System.out.println("Hub Sending: \n" + displayScreen + "\n\n");
+            // *Final Screen*
+            send(UI_FINAL, SCREEN);
+            System.out.println("Hub Sending: \n" + UI_FINAL + "\n\n");
             Thread.sleep(3000);
         }
     }
 
-    // Send data to all devices
+    // Send data to a recipient
     public void send(String msg, int recipient){
         devices.get(recipient).send(msg);
     }
@@ -118,14 +129,14 @@ public class hub {
         }
     }
     
-    // Start a connection to a device
+    // Start a connection to a specified device
     public void ConnectToDevice(int connector) throws UnknownHostException, IOException {
         int type; // deviceType for ChooseDevice (client role)
         switch (connector) {
-            case 2:  // Flowmeter
+            case FLOW_METER:
                 type = 2; // ControlPort (client)
                 break;
-            case 4:  // Hose
+            case HOSE:
                 type = 4; // MonitorPort (client)
                 break;
             default: // Screen (1), CardReader (3), or anything else
@@ -140,6 +151,5 @@ public class hub {
 
     public static void main(String[] args) throws IOException, InterruptedException{
         new hub();
-        
     }
 }
